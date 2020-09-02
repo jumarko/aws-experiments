ef080fec1d1df871479040c86f94050ecda65544
Juraj Martinka <jumarko@gmail.com>
2020-09-02 08:39:25 +0200

    NOT WORKING YET - Experimenting with examples from https://awsbloglink.wordpress.com/2019/03/07/en-i-tried-creating-ec2-with-aws-cdk/

--- cdk/ec2-with-aws-cdk/lib/ec2-with-aws-cdk-stack.ts
+++ cdk/ec2-with-aws-cdk/lib/ec2-with-aws-cdk-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class Ec2WithAwsCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+ constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+   super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+   new ec2.CfnInstance(this, 'MyInstance', {
+       imageId: 'ami-07ad4b1c3af1ea214',
+       instanceType: 't3.micro',
+       keyName: 'kp_wai',
+       monitoring: false,
+       networkInterfaces: [
+         {
+           deviceIndex: "0",
+           associatePublicIpAddress: true,
+           subnetId: 'subnet-09fe229ff896761aa'
+         }
+       ]
+     }
+   );
+ }
 }
