db722e2ece448909657069d328bd3515a5e8696a
Juraj Martinka <jumarko@gmail.com>
2020-09-03 10:26:18 +0200

    cdk-workshop: Add lambda function.

--- cdk/cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk/cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,8 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // defines an AWS lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory - relative to project root
+      handler: 'hello.handler' // file is 'hello', function is 'handler'
+    })
   }
 }
