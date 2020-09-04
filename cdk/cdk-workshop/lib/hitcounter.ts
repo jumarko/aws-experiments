b73cad481547d9bd0b7c78d5fa23662d3110683b
Juraj Martinka <jumarko@gmail.com>
2020-09-04 17:19:14 +0200

    cdk-workshop: Define HitConter construct.
    DynamoDB table and handler for the hitcounter.js lambda

--- cdk/cdk-workshop/lib/hitcounter.ts
+++ cdk/cdk-workshop/lib/hitcounter.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import {AttributeType} from '@aws-cdk/aws-dynamodb';
 
 export interface HitCounterProps {
     /** the function for which we want to count url hits **/
@@ -7,9 +9,26 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends cdk.Construct {
+    /** allows accessing the counter function */
+    public readonly handler: lambda.Function;
+
     constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
         super(scope, id);
 
-        // TODO
+        const table = new dynamodb.Table(this, 'Hits', {
+            partitionKey: {name: 'path', type: AttributeType.STRING}
+        });
+
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_10_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment: {
+                // functionName and tableName are "late-bound values"
+                // - they only resolve when we deploy our stack
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: table.tableName
+            }
+        });
     }
-}                                k
+}
